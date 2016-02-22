42b29245016815c1fd306ea8214b59eda077ba5f
Shalini Dangi <shalinidangi@ucla.edu>
2016-02-22 00:55:34 -0800

    implemented 'ospfs_write' and cleaned up 'ospfs_read'

--- lab3/ospfsmod.c
+++ lab3/ospfsmod.c
@@ -919,6 +919,12 @@ ospfs_read(struct file *filp, char __user *buffer, size_t count, loff_t *f_pos)
 	// Change 'count' so we never read past the end of the file.
 	/* EXERCISE: Your code here */
 
+	if (*f_pos >= oi->oi_size)
+	{
+		// File position is already past end of file
+		count = 0;
+	}
+
 	if (count + *f_pos > oi->oi_size)
 	{
 		// If we requested too much data, only allow the max
@@ -929,7 +935,8 @@ ospfs_read(struct file *filp, char __user *buffer, size_t count, loff_t *f_pos)
 	// Copy the data to user block by block
 	while (amount < count && retval >= 0) {
 		uint32_t blockno = ospfs_inode_blockno(oi, *f_pos); //block no in file that contains fpos
-
+		uint32_t n;
+		uint32_t block_offset;
 		//num bytes between offset into this block and start of next block
 		char *data;
 
@@ -948,9 +955,19 @@ ospfs_read(struct file *filp, char __user *buffer, size_t count, loff_t *f_pos)
 		/* EXERCISE: Your code here */
 
 		// Determine how much data to read
-		uint32_t n = ((*f_pos)/OSPFS_BLKSIZE + 1) * OSPFS_BLKSIZE - *f_pos; 
-		int success = copy_to_user(buffer, *f_pos, n);
-		if (!success)
+		// n = ((*f_pos)/OSPFS_BLKSIZE + 1) * OSPFS_BLKSIZE - *f_pos; 
+		block_offset = *f_pos % OSPFS_BLKSIZE;
+		n = OSPFS_BLKSIZE - block_offset;
+
+		// If we don't need to read the entire block, lessen 
+		// the amount we read to the count remaining
+		if (n > count - amount)
+		{
+			n = count - amount;
+		}
+
+		
+		if(copy_to_user(buffer, data + block_offset, n) > 0)
 		{
 			retval = -EFAULT;
 			goto done;
@@ -993,15 +1010,30 @@ ospfs_write(struct file *filp, const char __user *buffer, size_t count, loff_t *
 	// Support files opened with the O_APPEND flag.  To detect O_APPEND,
 	// use struct file's f_flags field and the O_APPEND bit.
 	/* EXERCISE: Your code here */
+	if (filp->f_flags & O_APPEND)
+	{
+		// Start writing at the end of the file
+		*f_pos = oi->oi_size;
+	}
 
 	// If the user is writing past the end of the file, change the file's
 	// size to accomodate the request.  (Use change_size().)
 	/* EXERCISE: Your code here */
 
+	if (count + *f_pos > oi->oi_size)
+	{
+		// If the write is too large (or the APPEND flag was indicated,
+		// change the file size to accomodate for it
+		change_size(oi, count + *f_pos);
+
+		// ** Do we need to check that change_size() was successful?**
+	}
+
 	// Copy data block by block
 	while (amount < count && retval >= 0) {
 		uint32_t blockno = ospfs_inode_blockno(oi, *f_pos);
 		uint32_t n;
+		unint32_t block_offset;
 		char *data;
 
 		if (blockno == 0) {
@@ -1016,8 +1048,23 @@ ospfs_write(struct file *filp, const char __user *buffer, size_t count, loff_t *
 		// read user space.
 		// Keep track of the number of bytes moved in 'n'.
 		/* EXERCISE: Your code here */
-		retval = -EIO; // Replace these lines
-		goto done;
+
+		// n = ((*f_pos)/OSPFS_BLKSIZE + 1) * OSPFS_BLKSIZE - *f_pos; 
+		block_offset = *f_pos % OSPFS_BLKSIZE;
+		n = OSPFS_BLKSIZE - block_offset;
+
+		// If we don't need to write to the entire block, lessen 
+		// the amount we write to the count remaining
+		if (n > count - amount)
+		{
+			n = count - amount;
+		}
+
+		if (copy_from_user(data + block_offset, buffer, n) > 0)
+		{
+			retval = -EFAULT;
+			goto done;
+		}
 
 		buffer += n;
 		amount += n;
